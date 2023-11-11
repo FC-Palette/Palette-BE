87cff6859c6931485d789130c80597d498af3feb
brotherstone97 <brotherstone97@gmail.com>
2023-11-12 01:02:57 +0900

    [#98] feat: 카테고리와 가격범위를 필터링하는 공동구매 repository메서드 선언

--- src/main/java/fc/server/palette/purchase/repository/PurchaseRepository.java
+++ src/main/java/fc/server/palette/purchase/repository/PurchaseRepository.java
@@ -2,15 +2,19 @@ package fc.server.palette.purchase.repository;
 
 
 import fc.server.palette.purchase.entity.Purchase;
+import fc.server.palette.purchase.entity.type.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
     @Modifying
     @Query("update Purchase p set p.isClosing=true where p.endDate < :now")
     int bulkUpdateClosing(@Param("now") LocalDate now);
+
+    List<Purchase> findAllByCategoryAndPriceBetween(Category category, Integer minPrice, Integer maxPrice);
 }
