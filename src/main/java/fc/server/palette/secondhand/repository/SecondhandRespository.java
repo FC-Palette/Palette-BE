68c82912a8afe3f3ef1f0c8dbe5870e8247be52c
brotherstone97 <brotherstone97@gmail.com>
2023-11-12 16:06:22 +0900

    [#98] feat: 중고거래 필터 repository 메서드 선언

--- src/main/java/fc/server/palette/secondhand/repository/SecondhandRespository.java
+++ src/main/java/fc/server/palette/secondhand/repository/SecondhandRespository.java
@@ -1,5 +1,6 @@
 package fc.server.palette.secondhand.repository;
 
+import fc.server.palette.purchase.entity.type.Category;
 import fc.server.palette.secondhand.entity.Secondhand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +11,6 @@ public interface SecondhandRespository extends JpaRepository<Secondhand, Long> {
 
     @Query("select s from Secondhand s where s.member.id = ?1 and s.id <> ?2")
     List<Secondhand> findAllByMemberIdAndExcludeId(Long memberId, Long idToExclude);
+
+    List<Secondhand> findAllByCategoryIn(List<Category> categories);
 }
